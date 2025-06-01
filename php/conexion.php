37cfc0e5053731db8d8a3910c6c4ee4b53af573f
Jannylgm736 <janny.garcia703@gmial.com>
2025-05-31 20:23:44 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php 
     $user="root"; /* usuario con el cual se accederá a la bd, en este caso se accedera con el usuario root */
-    $pass="root"; /*contraseña del usuario */
+    $pass="78910"; /*contraseña del usuario */
     $server="localhost";  /*localización del servidor, ahi puede ir también la ip del dispositivo que aloja la bd */
     $db="plf";   /* bd a la cual se quiere acceder*/
     $con= mysqli_connect($server,$user,$pass) or die("Error al conectar");      /* variable para hacer la conección contiene el servidor, el usuario y su contraseña*/
