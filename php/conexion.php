c0368317a4f66de6ca8234190b519be2d74e2873
AntonioGomez137 <133930767+AntonioGomez137@users.noreply.github.com>
2025-06-02 21:47:38 -0600

    Conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,10 @@
 <?php 
     $user="root"; /* usuario con el cual se accederá a la bd, en este caso se accedera con el usuario root */
-    $pass="root"; /*contraseña del usuario */
-    $server="localhost";  /*localización del servidor, ahi puede ir también la ip del dispositivo que aloja la bd */
+    $pass=""; /*contraseña del usuario */
+    $server="localhost";
+    $port="3308"; /*localización del servidor, ahi puede ir también la ip del dispositivo que aloja la bd */
     $db="plf";   /* bd a la cual se quiere acceder*/
-    $con= mysqli_connect($server,$user,$pass) or die("Error al conectar");      /* variable para hacer la conección contiene el servidor, el usuario y su contraseña*/
+    $con= mysqli_connect($server,$user,$pass, $port) or die("Error al conectar");      /* variable para hacer la conección contiene el servidor, el usuario y su contraseña*/
     mysqli_select_db($con,$db);     /* función que accede a la bd, como parametros recibe la conexión y la variable*/
     mysqli_set_charset($con,"utf8");     /* se especifica que la bd utiliza utf8*/
 ?>
--- php/controlador/vista_cliente.php
+++ php/controlador/vista_cliente.php
@@ -1,4 +1,8 @@
 <?php
+/* ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL & ~E_DEPRECATED); */
+
 header('Content-Type: text/html; charset=UTF-8');    /* Especificamos que se utulizara html con utf8 */
 date_default_timezone_set('America/Mexico_City');    /* Especificamos la zona horaria */
 
